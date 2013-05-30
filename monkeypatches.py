26907aed976e0921cf7d677f5801d2d72b62b5aa
Chris Wilson <chris+github@qwirx.com>
2013-05-30 17:57:58 +0100

    Add patches for get_deleted_objects method on ModelAdmin.

--- monkeypatches.py
+++ monkeypatches.py
@@ -12,7 +12,7 @@ from django.template.response import TemplateResponse
 from django.test.client import ClientHandler, RequestFactory, MULTIPART_CONTENT, \
     urlparse, FakePayload
 from django.test.utils import ContextList
-from aptivate_monkeypatch.monkeypatch import before, after, patch
+from aptivate_monkeypatch.monkeypatch import before, after, patch, insert
 from pprint import PrettyPrinter
 import django.template.loader
 
@@ -670,3 +670,104 @@ def select_related_descend_without_different_dbs(original_function,
     if field.model.
 ')
 """
+
+# Add a patch to ModelAdmin to allow getting a list of related objects
+# to be deleted.
+from django.contrib.admin.options import ModelAdmin
+@insert(ModelAdmin, 'get_deleted_objects')
+def ModelAdmin_get_deleted_objects(original_function, self, objs, opts,
+    request, using):
+    
+    """
+    Find all objects related to ``objs`` that should also be deleted. ``objs``
+    must be a homogenous iterable of objects (e.g. a QuerySet).
+
+    Returns a nested list of strings suitable for display in the
+    template with the ``unordered_list`` filter.
+
+    By default this just passes the request on to 
+    django.contrib.admin.util.get_deleted_objects, but this method exists
+    to allow subclasses to override the permissions required to delete
+    things.
+    """
+    from django.contrib.admin.util import get_deleted_objects
+    return get_deleted_objects(objs, opts, request.user, self.admin_site, using)
+
+# Patch ModelAdmin.delete_view() to call the instance method 
+# get_deleted_objects() instead of the global function, to allow overriding it.
+from django.contrib.admin.options import csrf_protect_m
+from django.db import transaction
+@patch(ModelAdmin, 'delete_view')
+@csrf_protect_m
+@transaction.commit_on_success
+def ModelAdmin_delete_view_with_self_get_deleted_objects(self, request,
+    object_id, extra_context=None):
+    
+    "The 'delete' admin view for this model."
+    opts = self.model._meta
+    app_label = opts.app_label
+    
+    from django.contrib.admin.util import unquote # , flatten_fieldsets, get_deleted_objects, model_format_dict
+    obj = self.get_object(request, unquote(object_id))
+
+    if not self.has_delete_permission(request, obj):
+        from django.core.exceptions import PermissionDenied
+        raise PermissionDenied
+
+    from django.utils.encoding import force_unicode
+    from django.utils.html import escape
+    if obj is None:
+        from django.http import Http404 
+        raise Http404(_('%(name)s object with primary key %(key)r does not exist.') % {'name': force_unicode(opts.verbose_name), 'key': escape(object_id)})
+
+    from django.db import router
+    using = router.db_for_write(self.model)
+
+    # Populate deleted_objects, a data structure of all related objects that
+    # will also be deleted.
+    (deleted_objects, perms_needed, protected) = self.get_deleted_objects(
+        [obj], opts, request, using)
+
+    if request.POST: # The user has already confirmed the deletion.
+        if perms_needed:
+            raise PermissionDenied
+        
+        obj_display = force_unicode(obj)
+        self.log_deletion(request, obj, obj_display)
+        self.delete_model(request, obj)
+
+        self.message_user(request, _('The %(name)s "%(obj)s" was deleted successfully.') % {'name': force_unicode(opts.verbose_name), 'obj': force_unicode(obj_display)})
+
+        from django.http import HttpResponseRedirect
+        from django.core.urlresolvers import reverse
+        if not self.has_change_permission(request, None):
+            return HttpResponseRedirect(reverse('admin:index',
+                                                current_app=self.admin_site.name))
+        return HttpResponseRedirect(reverse('admin:%s_%s_changelist' %
+                                    (opts.app_label, opts.module_name),
+                                    current_app=self.admin_site.name))
+
+    object_name = force_unicode(opts.verbose_name)
+
+    if perms_needed or protected:
+        title = _("Cannot delete %(name)s") % {"name": object_name}
+    else:
+        title = _("Are you sure?")
+
+    context = {
+        "title": title,
+        "object_name": object_name,
+        "object": obj,
+        "deleted_objects": deleted_objects,
+        "perms_lacking": perms_needed,
+        "protected": protected,
+        "opts": opts,
+        "app_label": app_label,
+    }
+    context.update(extra_context or {})
+
+    return TemplateResponse(request, self.delete_confirmation_template or [
+        "admin/%s/%s/delete_confirmation.html" % (app_label, opts.object_name.lower()),
+        "admin/%s/delete_confirmation.html" % app_label,
+        "admin/delete_confirmation.html"
+    ], context, current_app=self.admin_site.name)
